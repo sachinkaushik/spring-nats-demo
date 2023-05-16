82688a388a6e3c4c082671f1738ff45338dadf29
Sachin Sharma <sachin.kumar.sharma@intel.com>
2023-05-16 15:50:22 +0530

    first commit

---
+++ src/main/java/com/poc/NatsExampleApplication.java
@@ -0,0 +1,31 @@
+package com.poc;
+
+import io.nats.client.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.charset.StandardCharsets;
+
+@SpringBootApplication
+public class NatsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatsExampleApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+		return args -> {
+			Connection connect = Nats.connect();
+			Dispatcher dispatcher = connect.createDispatcher(message -> {
+				System.out.println(String.format("Received Message = [%s] from subject = [%s]",
+						new String(message.getData(), StandardCharsets.UTF_8), message.getSubject()));
+
+			//----lodfgdfg
+			});
+			dispatcher.subscribe("cmd.short");
+		};
+	}
+}
